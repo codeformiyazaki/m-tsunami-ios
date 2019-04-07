6291c58bf55ecda474db38ae5018d0782b5878df
Yosei Ito <y-itou@lumber-mill.co.jp>
2019-04-07 09:45:12 +0900

    MapViewによるマップを追加

--- iosapp/06-earthquake/MapViewController.swift
+++ iosapp/06-earthquake/MapViewController.swift
@@ -1,28 +1,32 @@
 //
-//  SecondViewController.swift
+//  MapViewController.swift
 //  06-earthquake
 //
-//  Created by Yosei Ito on 2019/01/20.
+//  Created by Yosei Ito on 2019/04/06.
 //  Copyright © 2019 LmLab.net. All rights reserved.
 //
 
 import UIKit
-import WebKit
+import MapKit
 
 class MapViewController: UIViewController {
+    @IBOutlet weak var mapView: MKMapView!
 
-    @IBOutlet weak var webView: WKWebView!
+    let lm = CLLocationManager()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // http://bit.ly/2FzKzMp
-        guard let url = URL(string: "https://kenzkenz.xsrv.jp/aaa/#13/131.44455/31.92127%3FS%3D1%26L%3D%5B%5B%7B%22id%22%3A%22shinsuishin%22%2C%22o%22%3A1%7D%2C%7B%22id%22%3A1%2C%22o%22%3A1%7D%5D%2C%5B%7B%22id%22%3A2%2C%22o%22%3A1%2C%22c%22%3A%22%22%7D%5D%2C%5B%7B%22id%22%3A4%2C%22o%22%3A1%2C%22c%22%3A%22%22%7D%5D%2C%5B%7B%22id%22%3A5%2C%22o%22%3A1%2C%22c%22%3A%22%22%7D%5D%5D") else {
-            return
-        }
-        print("loaded!")
-        let request = URLRequest(url: url)
-        webView.load(request)
+
+        lm.requestWhenInUseAuthorization()
+        lm.startUpdatingLocation()
     }
 
-
+    override func viewDidAppear(_ animated: Bool) {
+        var r = mapView.region
+        if let cood = lm.location?.coordinate {
+            r.center = cood
+        }
+        r.span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        mapView.setRegion(r, animated: true)
+    }
 }
-
