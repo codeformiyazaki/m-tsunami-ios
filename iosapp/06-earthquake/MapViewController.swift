a6766363b1a400e618155715658df2d787682cbb
Yosei Ito <y-itou@lumber-mill.co.jp>
2019-05-05 18:38:51 +0900

    bonsai

--- iosapp/06-earthquake/MapViewController.swift
+++ iosapp/06-earthquake/MapViewController.swift
@@ -12,30 +12,24 @@ import MapKit
 class MapViewController: UIViewController,MKMapViewDelegate {
     @IBOutlet weak var mapView: MKMapView!
     var csvLines = [String]()
-    
+
     let lm = CLLocationManager()
     var route:MKRoute?
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         lm.requestWhenInUseAuthorization()
         lm.startUpdatingLocation()
-        guard let path = Bundle.main.path(forResource:"buildings_locations", ofType:"csv") else {
-            print("csvファイルがないよ")
-            return
-        }
+        let path = Bundle.main.path(forResource:"buildings_locations", ofType:"csv")!
         let csvString = try! String(contentsOfFile: path, encoding: String.Encoding.utf8)
         csvLines = csvString.components(separatedBy: .newlines)
     }
-    
-    
-    
+
     override func viewDidAppear(_ animated: Bool) {
         var r = mapView.region
         if let cood = lm.location?.coordinate {
             r.center = cood
-            
         }
         r.span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
         mapView.setRegion(r, animated: true)
@@ -52,12 +46,9 @@ class MapViewController: UIViewController,MKMapViewDelegate {
             pa.title = row[1]
             pa.subtitle = "標高" + row[3] + "m " + row[4] + " " + row[5] + "階"
             mapView.addAnnotation(pa)
-            
-            print(row[0]+": "+row[1])
         }
-        
-        
     }
+
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
         if annotation is MKUserLocation {
             return nil
@@ -72,14 +63,14 @@ class MapViewController: UIViewController,MKMapViewDelegate {
         }
         return av
     }
-    
+
     func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
         let renderer = MKPolylineRenderer(overlay: overlay)
         renderer.strokeColor = UIColor.blue
         renderer.lineWidth = 4.0
         return renderer
     }
-    
+
     func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView){
         guard let src = lm.location?.coordinate else { return }
         guard let dst = view.annotation?.coordinate else { return }
@@ -94,22 +85,21 @@ class MapViewController: UIViewController,MKMapViewDelegate {
         let directions = MKDirections(request: directionRequest)
         directions.calculate { (response, error) in
             guard let directionResponse = response else {
-                if let error = error {
-                    print("we have error getting directions==\(error.localizedDescription)")
-                }
+                guard let e = error else { return }
+                print("we have error getting directions==\(e.localizedDescription)")
                 return
             }
+            // 以前の経路があれば消す
             if let prev_route = self.route {
                 self.mapView.removeOverlay(prev_route.polyline)
             }
+            // 新しい経路（とりあえず最初の候補）
             let new_route = directionResponse.routes[0]
-            
             self.mapView.addOverlay(new_route.polyline, level: .aboveRoads)
             self.route = new_route
-            //縮尺を設定
-            let rect = new_route.polyline.boundingMapRect
+            // 現在地と目的地が収まるよう縮尺変更
+            let rect = new_route.polyline.boundingMapRect.insetBy(dx: -600, dy: -600)
             self.mapView.setRegion(MKCoordinateRegion(rect),animated: true)
         }
-        
     }
 }
