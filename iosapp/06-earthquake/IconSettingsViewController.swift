a126093768ce546fd90a6b15a63454c9101585e0
koogawa <koogawa.app@gmail.com>
2019-10-27 00:54:37 +0900

    アイコン表示設定用の画面を作成

---
+++ iosapp/06-earthquake/IconSettingsViewController.swift
@@ -0,0 +1,43 @@
+//
+//  IconSettingsViewController.swift
+//  06-earthquake
+//
+//  Created by koogawa on 2019/10/27.
+//  Copyright © 2019 LmLab.net. All rights reserved.
+//
+
+import UIKit
+
+class IconSettingsViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+}
