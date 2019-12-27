39038216bbb80fc3ce0fdf5e3e78f12d9ecceada
koogawa <koogawa.app@gmail.com>
2019-12-28 00:17:25 +0900

    Firebase Setup

---
+++ iosapp/Pods/Target Support Files/Pods-06-earthquakeTests/Pods-06-earthquakeTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_06_earthquakeTests : NSObject
+@end
+@implementation PodsDummy_Pods_06_earthquakeTests
+@end
---
+++ iosapp/Pods/Target Support Files/Pods-06-earthquakeUITests/Pods-06-earthquakeUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_06_earthquakeUITests : NSObject
+@end
+@implementation PodsDummy_Pods_06_earthquakeUITests
+@end
