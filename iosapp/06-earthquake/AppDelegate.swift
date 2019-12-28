5f32a09ce25261a094c16e525005fc4121eacdb9
koogawa <koogawa.app@gmail.com>
2019-12-28 15:26:55 +0900

    Setup Firebase

--- iosapp/06-earthquake/AppDelegate.swift
+++ iosapp/06-earthquake/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import UserNotifications
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -18,6 +19,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         registerForPushNotifications()
+        FirebaseApp.configure()
+
         return true
     }
 
---
+++ iosapp/Pods/Target Support Files/Pods-06-earthquake/Pods-06-earthquake-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_06_earthquake : NSObject
+@end
+@implementation PodsDummy_Pods_06_earthquake
+@end
