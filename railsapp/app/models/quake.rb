a0a42bc5662b7276d1c01c57f3ba169206e8cc63
Yosei Ito <y-itou@lumber-mill.co.jp>
2020-01-02 21:47:39 +0900

    [rails]Set threshold number of earthquakes to 3. If there will be 3 or more quakes, notification will be sent.

--- railsapp/app/models/quake.rb
+++ railsapp/app/models/quake.rb
@@ -1,12 +1,13 @@
 class Quake < ApplicationRecord
   belongs_to :device
-  
+  P_THRESH = 0 # P波の検出下限
+  S_THRESH = 0 # S波の検出下限
+  N_THRESH = 3 # 10分以内にこの回数以上の振動を検知した場合、地震発生とみなす
+
   # Return true if receives certain alerts more than 3 times in 10 minites.
-  def self.happen?
-    p_thresh = 0
-    s_thresh = 0
-    t_border = Time.now - 10.minute
-    n = Quake.where("p > #{p_thresh} and s > #{s_thresh} and created_at > '#{t_border}'").count
-    return n >= 3
+  def self.happen?(now)
+    t_border = (now || Time.now) - 10.minute
+    n = Quake.where("p > #{P_THRESH} and s > #{S_THRESH} and created_at > '#{t_border}'").count
+    return n >= N_THRESH
   end
 end
