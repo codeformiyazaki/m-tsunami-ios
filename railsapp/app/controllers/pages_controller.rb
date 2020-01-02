639ff8f67514e022f60ff9ad34c0e7e3885e6b30
Yosei Ito <y-itou@lumber-mill.co.jp>
2020-01-02 20:11:17 +0900

    [rails]Added token method to Device model.

--- railsapp/app/controllers/pages_controller.rb
+++ railsapp/app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
   def root
+    @n_apn_tokens = ApnToken.count
+    @n_devices = Device.active.count
   end
 end
--- railsapp/app/models/device.rb
+++ railsapp/app/models/device.rb
@@ -1,2 +1,8 @@
 class Device < ApplicationRecord
+  validates :name, presence: true
+  scope :active, -> { where(active: true) }
+
+  def token
+    id.to_s.crypt("mtsunami")
+  end
 end
